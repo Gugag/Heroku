8faae56110f06f8b88f65363a78ce07ecaceed64
GugaGordeladze <gugagordeladze@yahoo.com>
2024-05-17 15:40:34 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 49890;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ app.post('/sendSMS', async (req, res) => {
     const response = await fetch('https://api.twilio.com/2010-04-01/Accounts/ACd2799421f889c5a61dc6dc8ed5c31dec/Messages.json', {
         method: 'POST',
         headers: {
-            'Authorization': 'Basic ' + Buffer.from('ACd2799421f889c5a61dc6dc8ed5c31dec:2d662aaf6b325c9fb9dfce76f356de15').toString('base64'),
+            'Authorization': 'Basic ' + Buffer.from('ACd2799421f889c5a61dc6dc8ed5c31dec:2225af7e5b6ab7a039111f419b806814').toString('base64'),
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: new URLSearchParams({
